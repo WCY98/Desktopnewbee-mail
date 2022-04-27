36f8ce51ca741d468a2dad353deb1c7ed2ed44f5
WCY98 <wangchengyu0324@gmail.com>
2022-04-28 06:46:25 +0800

    GoodsQA and GoodsQANumAndPage

--- src/main/java/ltd/newbee/mall/newbeemall/entity/GoodsQA.java
+++ src/main/java/ltd/newbee/mall/newbeemall/entity/GoodsQA.java
@@ -4,69 +4,152 @@ import java.util.Date;
 
 import javax.annotation.Generated;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public class GoodsQA {
-	private Long goodsId;
+	@Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5959275+08:00", comments="Source field: questions.question_id")
+    private Long questionId;
 
-	private String questionContents;
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.user_id")
+    private Long userId;
 
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private Date questionDate;
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.question_date")
+    private Date questionDate;
 
-	private String answerContents;
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.goods_id")
+    private Long goodsId;
 
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private Date answerDate;
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.proved")
+    private Integer proved;
 
-	private Integer count;
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.question_contents")
+    private String questionContents;
 
-	public Long getGoodsId() {
-		return goodsId;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5959275+08:00", comments="Source field: questions.question_id")
+    public Long getQuestionId() {
+        return questionId;
+    }
 
-	public void setGoodsId(Long goodsId) {
-		this.goodsId = goodsId;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.question_id")
+    public void setQuestionId(Long questionId) {
+        this.questionId = questionId;
+    }
 
-	public String getQuestionContents() {
-		return questionContents;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.user_id")
+    public Long getUserId() {
+        return userId;
+    }
 
-	public void setQuestionContents(String questionContents) {
-		this.questionContents = questionContents;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.user_id")
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
-	public Date getQuestionDate() {
-		return questionDate;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.question_date")
+    public Date getQuestionDate() {
+        return questionDate;
+    }
 
-	public void setQuestionDate(Date questionDate) {
-		this.questionDate = questionDate;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.question_date")
+    public void setQuestionDate(Date questionDate) {
+        this.questionDate = questionDate;
+    }
 
-	public String getAnswerContents() {
-		return answerContents;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.goods_id")
+    public Long getGoodsId() {
+        return goodsId;
+    }
 
-	public void setAnswerContents(String answerContents) {
-		this.answerContents = answerContents;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.goods_id")
+    public void setGoodsId(Long goodsId) {
+        this.goodsId = goodsId;
+    }
 
-	public Date getAnswerDate() {
-		return answerDate;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.proved")
+    public Integer getProved() {
+        return proved;
+    }
 
-	public void setAnswerDate(Date answerDate) {
-		this.answerDate = answerDate;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.proved")
+    public void setProved(Integer proved) {
+        this.proved = proved;
+    }
 
-	public Integer getCount() {
-		return count;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.question_contents")
+    public String getQuestionContents() {
+        return questionContents;
+    }
 
-	public void setCount(Integer count) {
-		this.count = count;
-	}
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:19:28.5969275+08:00", comments="Source field: questions.question_contents")
+    public void setQuestionContents(String questionContents) {
+        this.questionContents = questionContents == null ? null : questionContents.trim();
+    }
+    
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_id")
+    private Long answerId;
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.employee_id")
+    private Long employeeId;
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_date")
+    private Date answerDate;
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_contents")
+    private String answerContents;
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_id")
+    public Long getAnswerId() {
+        return answerId;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_id")
+    public void setAnswerId(Long answerId) {
+        this.answerId = answerId;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.employee_id")
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.employee_id")
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_date")
+    public Date getAnswerDate() {
+        return answerDate;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_date")
+    public void setAnswerDate(Date answerDate) {
+        this.answerDate = answerDate;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_contents")
+    public String getAnswerContents() {
+        return answerContents;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:22:57.4938757+08:00", comments="Source field: answer.answer_contents")
+    public void setAnswerContents(String answerContents) {
+        this.answerContents = answerContents == null ? null : answerContents.trim();
+    }
+    
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:23:32.6818884+08:00", comments="Source field: qa_like.like_date")
+    private Date likeDate;
+ 
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:23:32.6818884+08:00", comments="Source field: qa_like.like_date")
+    public Date getLikeDate() {
+        return likeDate;
+    }
+
+    @Generated(value="org.mybatis.generator.api.MyBatisGenerator", date="2022-04-27T10:23:32.6818884+08:00", comments="Source field: qa_like.like_date")
+    public void setLikeDate(Date likeDate) {
+        this.likeDate = likeDate;
+    }
+
+
 
 }
